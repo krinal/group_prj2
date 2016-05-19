bb62379fe73eb07de8273ec3b14c18b6bab85e95
Julia Kim <jkim0620@gmail.com>
2016-05-19 01:29:02 -0400

    modify main css

---
+++ index.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+	
+
+
+
+// 	function imageShow() {
+// 		var images = ["red", "blue", "pink"];
+
+// 		for (i = 0; i < images.length; i++) {
+// 				$("#slideShow").css("background-color", images[i]);
+// 			}
+// 			setTimeout(imageShow, 5000);
+// 		}
+
+// 	imageShow();
+
+// })
