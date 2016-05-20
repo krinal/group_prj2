60f3f9fde292a3cf15eeeb77cfed82aee0d3f501
Julia Kim <jkim0620@gmail.com>
2016-05-20 00:38:19 -0400

    add jquery effect to index page

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 	
 	// $(".requestButton").mouseover(function() {
-	// 	$(this).slideUp()
+	// 	$(".opening").fadeOut(800)
 	// });
-	// $(".requestButton").mouseover(function() {
-	// 	$("p.opening").slideDown()
-	// });
+
+	$(".opening").mouseover(function() {
+		$(this).slideUp(1000)
+	});
+
+	$(".opening").mouseover(function() {
+		$(".requestButton").fadeIn(1000)
+	});
+
+	$(".requestButton").mouseleave(function() {
+		$(this).fadeOut(1000)
+	});
+
+	$(".requestButton").mouseleave(function() {
+		$(".opening").slideDown(1000)
+	});
+
+
 
 	$(".styleForm3").click(function() {
 		alert("Confirmed!")
